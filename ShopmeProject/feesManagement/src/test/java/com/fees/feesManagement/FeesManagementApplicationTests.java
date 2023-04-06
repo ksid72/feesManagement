f6133e3f8128aa3362a1a39e7bac411b9728dedd
ksid72 <ksid72@gmail.com>
2023-04-07 01:49:37 +0530

    second

---
+++ ShopmeProject/feesManagement/src/test/java/com/fees/feesManagement/FeesManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fees.feesManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeesManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
